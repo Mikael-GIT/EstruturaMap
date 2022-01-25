300784bb2fd09d66ea35ac92dc938fe1cf82c1c0
Mikael Tavares <mikael.tavares@unigranrio.br>
2022-01-25 00:29:02 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Map<String, String> cookies = new TreeMap<>();
+        cookies.put("username", "Maria");
+        cookies.put("email", "maria@gmail.com");
+        cookies.put("phone", "2197213982");
+
+
+        cookies.remove("email");
+        cookies.put("phone", "2039233");
+
+        System.out.println("Contains 'phone' key: " + cookies.containsKey("phone"));
+        System.out.println("Phone number: " + cookies.get("phone"));
+        System.out.println("Email: " + cookies.get("email"));
+        System.out.println("Size: " + cookies.size());
+        
+        
+        System.out.println("All cookies: ");
+        for (String key : cookies.keySet()) { //keySet retorna um Set com as chaves do map
+            System.out.println(key + ": " + cookies.get(key));
+        }
+    }
+}
